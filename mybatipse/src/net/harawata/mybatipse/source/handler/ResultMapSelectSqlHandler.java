abf6780ab3a8922ea55d4a9ef953b4f08f8edbe9
kdavidson <ken.j.davidson@live.ca>
2018-09-25 11:24:42 -0400

    Added Mapper Source options for SQL blocks
    Add <sql> blocks for insert, update and select columns based on the
    selected result set.

---
+++ mybatipse/src/net/harawata/mybatipse/source/handler/ResultMapSelectSqlHandler.java
@@ -0,0 +1,67 @@
+/*-******************************************************************************
+ * Copyright (c) 2018 Sc122.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Sc122 - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+
+package net.harawata.mybatipse.source.handler;
+
+import javax.xml.xpath.XPathExpressionException;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import net.harawata.mybatipse.util.XpathUtil;
+
+/**
+ * <sql id="selectResultMap"></sql>
+ * 
+ * @author kdavidson
+ */
+public class ResultMapSelectSqlHandler extends ResultMapSqlSourceHandler
+{
+
+	@Override
+	protected String buildSqlId(Element resultMap)
+	{
+		return resultMap.getAttribute("id").replaceAll("Map", "") + "SelectColumns";
+	}
+
+	@Override
+	protected String buildSqlStatement(Element resultMap)
+	{
+		final StringBuilder sql = new StringBuilder("\n");
+
+		try
+		{
+			NodeList nodes = XpathUtil.xpathNodes(resultMap, "id|result");
+
+			for (int i = 0; i < nodes.getLength(); i++)
+			{
+				Node node = nodes.item(i).getAttributes().getNamedItem("column") != null
+					? nodes.item(i).getAttributes().getNamedItem("column")
+					: nodes.item(i).getAttributes().getNamedItem("property");
+
+				if (node != null)
+				{
+					sql.append(node.getNodeValue());
+					if (i < nodes.getLength() - 1)
+						sql.append(",\n");
+				}
+			}
+		}
+		catch (XPathExpressionException e)
+		{
+			// no child nodes
+		}
+
+		return sql.toString();
+	}
+
+}
